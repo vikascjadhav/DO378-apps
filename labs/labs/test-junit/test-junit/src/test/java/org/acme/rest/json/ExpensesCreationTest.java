f3a1ff55d3591fc1bdc7a17d5e9365d1f9531ef4
Student User <student@workstation.lab.example.com>
2022-12-30 13:24:28 -0500

    test-junit lab setup

---
+++ labs/labs/test-junit/test-junit/src/test/java/org/acme/rest/json/ExpensesCreationTest.java
@@ -0,0 +1,12 @@
+package org.acme.rest.json;
+
+import org.junit.jupiter.api.Test;
+
+public class ExpensesCreationTest {
+   
+    @Test
+    public void testCreateExpense() {
+
+    }
+
+}
---
+++ labs/solutions/test-junit/test-junit/src/test/java/org/acme/rest/json/NativeExpensesCreationIT.java
@@ -0,0 +1,7 @@
+package org.acme.rest.json;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeExpensesCreationIT extends ExpensesCreationTest {
+}
