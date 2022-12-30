39d42d88105a6e663c24439b4f652005fa6d48c6
Student User <student@workstation.lab.example.com>
2022-12-30 13:23:57 -0500

    native-build lab setup

---
+++ labs/labs/native-build/native-build/expense-service/src/main/java/org/acme/rest/json/ExpenseConfiguration.java
@@ -0,0 +1,20 @@
+package org.acme.rest.json;
+
+import java.math.BigDecimal;
+
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+
+import io.quarkus.arc.config.ConfigProperties;
+
+@ConfigProperties(prefix = "expense")
+public interface ExpenseConfiguration {
+    BigDecimal getMaxSingleExpenseAmount();
+    BigDecimal getMaxMonthlyExpenseAmount();
+
+    @ConfigProperty(defaultValue = "Expense amount is larger than the limit (%s)")
+    String getSingleLimitErrorMsg();
+
+    @ConfigProperty(
+        defaultValue = "If expense is accepted, it would exceed the monthly expense limit (%s)")
+    String getMonthlyLimitErrorMsg();
+}
---
+++ labs/solutions/native-build/native-build/expense-service/src/test/java/org/acme/rest/json/ExpenseResourceTest.java
@@ -0,0 +1,8 @@
+package org.acme.rest.json;
+
+import io.quarkus.test.junit.QuarkusTest;
+
+@QuarkusTest
+public class ExpenseResourceTest {
+
+}
