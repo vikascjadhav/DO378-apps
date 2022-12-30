757a779fe5d0e3a1b02496e286c83bb0b372034d
Student User <student@workstation.lab.example.com>
2022-12-30 12:54:07 -0500

    app-connect lab setup

---
+++ labs/1.sh
@@ -0,0 +1,18 @@
+
+#!/usr/bin/bash
+filename="$1"
+while read -r line; do
+ name="$line"
+ echo "Setting lab $name"
+ lab $name start
+ echo "Lab Setup Done"
+ echo " copying lab files to git dir"
+ cp  -r /home/student/DO378/labs/$name ./labs/$name
+ cp  -r /home/student/DO378/solutions/$name ./solutions/$name
+ echo "Pushing to git repository"
+ git add *
+ git commit -m "$name lab setup"
+ git push
+ echo delete lab
+ lab $name finish
+done < "$filename"
